5a6c7ef46430504ff40eaa070607fc0ae401bc01
웹스토리보이 <144635651+chfhr22@users.noreply.github.com>
2023-12-15 09:34:09 +0900

    12-15

---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import{BrowserRouter} from "react-router-dom"
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './assets/scss/style.scss';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
+);
+
